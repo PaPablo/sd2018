1da74b1884db03ebe4e27ee06d21506c874eab17
LucianoFromTrelew <lucianoserruya@gmail.com>
2018-09-13 17:00:59 -0300

    Agregar CLI para el cliente RMI

--- tp2/ej1/src/CLICliente.java
+++ tp2/ej1/src/CLICliente.java
@@ -12,7 +12,8 @@ public class CLICliente {
             n1 = Integer.parseInt(args[1]);
             n2 = Integer.parseInt(args[3]);
         } catch (NumberFormatException e) {
-            handleError();
+            System.out.println("Necesita ingresar dos números y un operador");
+            System.exit(1);
         }
 
         Cliente cliente = new Cliente(host);
@@ -33,12 +34,12 @@ public class CLICliente {
                     res = cliente.dividir(n1, n2);
                     break;
                 default:
-                    System.out.println("Operador incorrecto\n");
+                    System.out.println("Operador incorrecto");
                     System.exit(1);
             }
 
             System.out.println(String.format(
-                        "El resultado de la operación [%d %s %d] es igual a %d\n",
+                        "El resultado de la operación [%d %s %d] es igual a %d",
                         n1,
                         op,
                         n2,
@@ -52,7 +53,7 @@ public class CLICliente {
     }
     public static void printHelp() {
         System.out.println(
-                "USAGE: java CLICliente <SERVER_HOST> <N1> <OP> <N2>\n"
+                "USAGE: java CLICliente <SERVER_HOST> <N1> <OP> <N2>"
                 );
     }
     public static void handleError() {
