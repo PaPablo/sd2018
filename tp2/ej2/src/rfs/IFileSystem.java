ab3e903680a5e00ef26e73f460b73b9d5b1a172c
LucianoFromTrelew <lucianoserruya@gmail.com>
2018-09-18 09:38:35 -0300

    Modificar interfase de file system

--- tp2/ej2/src/rfs/IFileSystem.java
+++ tp2/ej2/src/rfs/IFileSystem.java
@@ -9,7 +9,7 @@ import java.rmi.RemoteException;
 
 public interface IFileSystem extends Remote {
 	public File open(String filename) throws RemoteException;
-	public int read(File file, byte[] buffer) throws RemoteException;
+	public byte[] read(File file, int count) throws RemoteException;
 	public int write(File file, byte[] data) throws RemoteException;
 	public boolean close(File file) throws RemoteException;
 }
