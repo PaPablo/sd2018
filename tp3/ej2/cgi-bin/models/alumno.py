84adf400ab343f2a1c9909cda4d5545c6b5f318f
LucianoFromTrelew <lucianoserruya@gmail.com>
2018-10-23 08:10:30 -0300

    Agregar Alumno
    Por lo pronto es solo cascarón de datos

---
+++ tp3/ej2/cgi-bin/models/alumno.py
@@ -0,0 +1,22 @@
+class Alumno(object):
+
+    """Clase para representar el alumno"""
+
+    def __init__(self, legajo, nombre, sexo, edad, password):
+        self.legajo = legajo
+        self.nombre = nombre
+        self.sexo = sexo
+        self.edad = edad
+        self.password = password
+
+    def __repr__(self):
+        return f"Alumno(\
+{self.legajo}, {self.nombre}, {self.sexo}, {self.edad}, {self.password})"
+
+    @classmethod
+    def from_row(cls, row):
+        """Devuelve un Alumno a partir de una tupla"""
+        try:
+            return cls(*row)
+        except TypeError as e:
+            return None
