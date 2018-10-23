239f573f8ddff84850ee07fb43ac7dbb79d516e7
LucianoFromTrelew <lucianoserruya@gmail.com>
2018-10-23 10:55:37 -0300

    Agregar script cascarón de login

--- tp3/ej2/cgi-bin/login.py
+++ tp3/ej2/cgi-bin/login.py
@@ -1,14 +1,29 @@
 #!/usr/bin/env python3
 
 import jinja2
+import os
+import cgi
+
 from utils.utils import print_headers, get_template
+from utils.logger import Logger
+
+def post():
+    """Loguear usuario"""
+    form = cgi.FieldStorage()
+    Logger.info(form)
+
+def get():
+    template = get_template("login.html")
+    print(template.render())
 
 def main():
     print_headers()
-    template = get_template("login.html")
-    user = {"username": "Luciano"}
-    print(template.render(is_logged_in=False,
-                          user=user))
+    req_method = os.getenv("REQUEST_METHOD")
+    Logger.info("EL METODO ES [{}]".format(req_method))
+    if req_method == "GET":
+        return get()
+    elif req_method == "POST":
+        return post()
 
 
 if __name__ == "__main__":
