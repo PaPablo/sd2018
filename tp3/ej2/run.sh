2006c60f0f449cebd5a0cff1f0bf2dde31ced5bd
LucianoFromTrelew <lucianoserruya@gmail.com>
2018-10-27 09:54:01 -0300

    Matar los servidores correctamente

--- tp3/ej2/run.sh
+++ tp3/ej2/run.sh
@@ -3,9 +3,12 @@
 trap ctrl_c int
 
 function ctrl_c() {
-    PID=$(ps aux | grep -i "proxy.py" | head -1 | cut -d " " -f4)
+    PID=$(ps aux | grep -i "proxy.py" | head -1 | cut -d " " -f3)
+    if [ ! $PID ]; then
+        PID=$(ps aux | grep -i "proxy.py" | head -1 | cut -d " " -f4)
+    fi
     echo "CTRL + C trapped, killing server [PID $PID]"
-    kill $PID || pkill python
+    kill $PID
 }
 
 ./proxy.py &
