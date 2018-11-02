60873e55695fd03a439b3769d656438d4490452c
LucianoFromTrelew <lucianoserruya@gmail.com>
2018-11-02 17:24:43 -0300

    Eliminar código comentado

--- tp3/ej5/cgi-bin/chat.py
+++ tp3/ej5/cgi-bin/chat.py
@@ -202,83 +202,6 @@ def get_active_users():
 
         except BlockingIOError as e:
             time.sleep(0.1)
-# def get_messages_from_lines(lines, starting=0):
-    # """Devuelve una lista de los mensajes con el usuario que lo escribió"""
-    # _messages = []
-    # for i, l in enumerate(lines):
-        # if i >= starting:
-            # l = l.replace("\n", "").replace("\r", "").split(":")
-            # l = [line.strip() for line in l]
-            # _messages.append({
-                # "user": l[0],
-                # "message": l[1]
-            # })
-
-    # return _messages
-
-# def get_main_page(template_name="index.html", user=None,
-                  # new_msg=None, starting=0):
-    # """Devuelve la página del chat"""
-    # while True:
-        # try:
-            # with open(CHATFILE, "a+") as f:
-                # # Bloquear archivo
-                # fcntl.flock(f, fcntl.LOCK_EX | fcntl.LOCK_NB)
-
-                # if new_msg and user:
-                    # f.write(f"{user}: {new_msg}\n")
-
-                # f.seek(0)
-
-                # messages = get_messages_from_lines(
-                    # f.readlines(), starting=starting)
-
-                # # Liberar archivo
-                # fcntl.flock(f, fcntl.LOCK_UN)
-                # return get_template(template_name).render(
-                    # user=user, users=get_all_users(), messages=messages)
-        # except BlockingIOError as e:
-            # log(f"BLOQUEADO [{e}]")
-            # time.sleep(0.1)
-
-# def get_user_or_create(username):
-    # """Devuelve el usuario registrado en el archivo o lo crea si no existe"""
-    # while True:
-        # try:
-            # with open(USERSFILE, "a+") as f:
-                # fcntl.flock(f, fcntl.LOCK_EX | fcntl.LOCK_NB)
-                # f.seek(0)
-                # for user in f.readlines():
-                    # user = user.replace("\n", "").replace("\r", "")
-                    # if user == username:
-                        # fcntl.flock(f, fcntl.LOCK_UN)
-                        # return user
-
-                # f.write(f"{username}\n")
-                # fcntl.flock(f, fcntl.LOCK_UN)
-                # return username
-        # except BlockingIOError as e:
-            # log(f"BLOQUEADO [{e}]")
-            # time.sleep(0.1)
-
-# def get_all_users():
-    # """Devuelve todos los usuarios registrados en el archivo"""
-    # users = []
-    # while True:
-        # try:
-            # with open(USERSFILE, "a+") as f:
-                # fcntl.flock(f, fcntl.LOCK_EX | fcntl.LOCK_NB)
-                # f.seek(0)
-                # for user in f.readlines():
-                    # user = user.replace("\n", "").replace("\r", "")
-                    # users.append(user)
-
-                # fcntl.flock(f, fcntl.LOCK_UN)
-                # return users
-        # except BlockingIOError as e:
-            # log(f"BLOQUEADO [{e}]")
-            # time.sleep(0.1)
-
 
 def get():
     session_id = get_cookie_value(COOKIE_NAME)
@@ -301,7 +224,6 @@ def get():
         # La sesión se tiene que actualizar haciendo
         # session["last_line"] += starting
         # Y devolverle al cliente los mensajes que no vio todavia
-        # que no sé cómo sacarlos, je
 
         delete_session(session)
         session["last_line"] = str(
@@ -342,7 +264,6 @@ def post():
         # Loguear
         log(f"post() - NO LOGUEADO")
         username = form.getvalue("username")
-        # user = get_user_or_create(username)
         session = create_session(username)
         if not session:
             return get_template("login.html").render(errors={
