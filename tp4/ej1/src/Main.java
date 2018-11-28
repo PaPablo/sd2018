623f54046326d34b6cdb09ede3d35cc708fa7738
Pablo <pabloatm980@gmail.com>
2018-11-28 02:09:34 -0300

    New main and fixed arguments on reader and writer

--- tp4/ej1/src/Main.java
+++ tp4/ej1/src/Main.java
@@ -5,6 +5,31 @@ import jade.wrapper.*;
 
 class Main {
     public static void main(String[] args) {
+
+        if (args.length < 2){
+            System.out.println("Necesito más comandos");
+            System.exit(0);
+        }
+
+        String command,src,dst;
+
+        
+        if (args[0] == "help") {
+            System.out.println("Argumentos posibles:");
+            System.out.println("write <source-file> <destination-file>");
+            System.out.println("read <source-file> <destination-file>");
+            System.exit(0);
+        } 
+        else if (args.length < 3){
+            System.exit(0);
+        }
+       // Get Command
+        command = args[0];
+
+        src = args[1];
+        dst = args[2];
+
+
         // Get a hold on JADE runtime
         Runtime rt = Runtime.instance();
         // Create a default profile
@@ -16,15 +41,32 @@ class Main {
         // and pass it a reference to an Object
 
         Object reference = new Object();
-        Object agentArgs[] = new Object[1];
+        Object agentArgs[] = new Object[2];
 
-        agentArgs[0]= new String("/home/pablo/archivito.txt");
+        agentArgs[0] = src;
+        agentArgs[1] = dst;
+
         try {
             //AgentController dummy = cc.createNewAgent("inProcess",
                     //"FileReader", agentArgs);
-            AgentController dummy = cc.createNewAgent("inProcess",
-                    "FileWriter", agentArgs);
+            String agent = null;
+
+            if (command.equals("write")) {
+                agent = "FileWriter";
+            }
+            else if (command.equals("read")) {
+                agent = "FileReader";
+            }
+            else {
+                System.out.println("Operación no válida");
+                System.out.println("Operaciones posibles: read | write");
+                System.exit(0);
+            }
+
             // Fire up the agent
+            AgentController dummy = cc.createNewAgent("inProcess",
+                    agent, agentArgs);
+
             dummy.start();
         } catch(Exception e){
             e.printStackTrace();
